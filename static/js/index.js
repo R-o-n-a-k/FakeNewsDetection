56a813c346d5b4d596b1c66bb95af1a3e516f5ad
Ronak Patel <rroonnaakk7@gmail.com>
2021-09-11 09:46:46 +0530

    Addition of Scroll Button

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+// ----------scroll button---------------
+    mybutton = document.getElementById("myBtn");
+    window.onscroll = function () { scrollFunction() };
+    function scrollFunction() {
+      if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
